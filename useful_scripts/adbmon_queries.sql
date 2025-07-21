7b51552f9795abb2951b3decfdc6280778df2d09
@vasiliev-a-v <vasiliev.av@aol.com>
2025-07-21 19:04:33 +0300

    20250721

--- useful_scripts/adbmon_queries.sql
+++ useful_scripts/adbmon_queries.sql
@@ -14,7 +14,7 @@ SET optimizer = off;
 
 
 SELECT to_char(now(), 'YYYY-MM-DD_HH24-MI-SS') date \gset
-\set f_ta_top gzip' '-f' '>' '/tmp/ta_top_ :date .csv.gz
+-- \set f_ta_top gzip' '-f' '>' '/tmp/ta_top_ :date .csv.gz
 \set f_ta_pg_stat_activity gzip' '-f' '>' '/tmp/ta_pg_stat_activity_ :date .csv.gz
 \set f_ta_gp_resgroup_status gzip' '-f' '>' '/tmp/ta_gp_resgroup_status_ :date .csv.gz
 \set f_ta_gp_resgroup_status_per_seg gzip' '-f' '>' '/tmp/ta_gp_resgroup_status_per_seg_ :date .csv.gz
@@ -22,13 +22,13 @@ SELECT to_char(now(), 'YYYY-MM-DD_HH24-MI-SS') date \gset
 \set f_ta_audit_mem_usage gzip' '-f' '>' '/tmp/ta_mem_usage_ :date .csv.gz
 
 
-\qecho :f_ta_top
-COPY (SELECT * FROM adbmon.t_audit_top
-       WHERE 1 = 1
-         AND dtm BETWEEN :'begin'
-                     AND :'end'
-   ) TO PROGRAM :'f_ta_top' (FORMAT CSV, HEADER)
-;
+-- \qecho :f_ta_top
+-- COPY (SELECT * FROM adbmon.t_audit_top
+       -- WHERE 1 = 1
+         -- AND dtm BETWEEN :'begin'
+                     -- AND :'end'
+   -- ) TO PROGRAM :'f_ta_top' (FORMAT CSV, HEADER)
+-- ;
 
 \qecho :f_ta_pg_stat_activity
 COPY (SELECT * FROM adbmon.t_audit_pg_stat_activity
